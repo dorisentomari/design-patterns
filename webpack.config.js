fd6b7f2003000f2a17cd84e079caadae0b43702e
Dawnight <weizhiqimail@foxmail.com>
2018-11-04 22:39:30 +0800

    添加使用webpack的插件html-webpack-plugin，然后使用webpack-dev-server进行本地服务热更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname,
     filename: './release/bundle.js'
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    // 根目录
+    contentBase: path.join(__dirname, './release'),
+    // 自动打开浏览器
+    open: true,
+    // 本地服务的端口
+    port: 9000
   }
 };
