5d1f4c76ccd750b7b8255c3cb63d8c17ee59629a
Dawnight <weizhiqimail@gmail.com>
2018-11-18 17:17:43 +0800

    面向对象思想

---
+++ docs/code/01-class的基本使用.js
@@ -0,0 +1,38 @@
+// 父类
+class People {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat () {
+    console.log(`${this.name} eat...`);
+  }
+  speak(){
+    console.log(`${this.name} speak....`);
+  }
+}
+
+let zhang = new People('zhang', 20);
+zhang.eat();
+zhang.speak();
+
+let wang = new People('wang', 21);
+wang.eat();
+wang.speak();
+
+// 子类
+class Student extends People {
+  constructor(name, age, number){
+    super(name, age);
+    this.number = number;
+  }
+  study () {
+    console.log(`${this.name} study...`);
+  }
+}
+
+let xiaoming = new Student('xiaoming', 18, 24);
+xiaoming.eat();
+xiaoming.speak();
+xiaoming.study();
+
